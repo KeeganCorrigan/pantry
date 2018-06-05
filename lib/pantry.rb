3116bf195e08b2d5a55f02c3bfe7a66564315769
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-06-05 09:51:45 -0600

    Finished and tested iteration_2

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,8 +1,12 @@
+require 'pry'
+
 class Pantry
-  attr_reader :stock
+  attr_reader :stock,
+              :shopping_list
 
   def initialize
     @stock = {}
+    @shopping_list = {}
   end
 
   def stock_check(food)
@@ -13,5 +17,18 @@ class Pantry
   def restock(food, quantity)
     @stock[food] += quantity if @stock.include?(food)
     @stock[food] = quantity if !@stock.include?(food)
+  end
+
+  def add_to_shopping_list(recipe)
+    recipe.ingredients.each do |food, quantity|
+      @shopping_list[food] += quantity if @shopping_list.include?(food)
+      @shopping_list[food] = quantity if !@shopping_list.include?(food)
+    end
+  end
+
+  def print_shopping_list
+    @shopping_list.inject('') do |list, (food, quantity)|
+        list << "* #{food}:" + " #{quantity}\n"
+    end.chomp
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -2,6 +2,7 @@ require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/recipe'
+require 'pry'
 
 class PantryTest < Minitest::Test
   def test_it_exists
@@ -28,10 +29,17 @@ class PantryTest < Minitest::Test
     assert_equal 30, pantry.stock_check("Cheese")
   end
 
+  def test_shopping_list_empty
+    pantry = Pantry.new
+    assert_equal Hash, pantry.shopping_list.class
+    assert_equal 0, pantry.shopping_list.length
+  end
+
   def test_it_adds_the_recipe_to_shopping_list
     r = Recipe.new("Cheese Pizza")
     r.add_ingredient("Cheese", 20)
     r.add_ingredient("Flour", 20)
+    pantry = Pantry.new
     pantry.add_to_shopping_list(r)
     expected = { "Cheese" => 20, "Flour" => 20 }
     assert_equal expected, pantry.shopping_list
@@ -41,6 +49,7 @@ class PantryTest < Minitest::Test
     r = Recipe.new("Cheese Pizza")
     r.add_ingredient("Cheese", 20)
     r.add_ingredient("Flour", 20)
+    pantry = Pantry.new
     pantry.add_to_shopping_list(r)
     r = Recipe.new("Spaghetti")
     r.add_ingredient("Spaghetti Noodles", 10)
@@ -55,6 +64,7 @@ class PantryTest < Minitest::Test
     r = Recipe.new("Cheese Pizza")
     r.add_ingredient("Cheese", 20)
     r.add_ingredient("Flour", 20)
+    pantry = Pantry.new
     pantry.add_to_shopping_list(r)
     r = Recipe.new("Spaghetti")
     r.add_ingredient("Spaghetti Noodles", 10)
