4e172bdb29d3637ab2a3f5f103d78883e2e7ccaa
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-06-05 11:36:31 -0600

    final commit

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,24 +1,25 @@
+# frozen_string_literal: true
+
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/recipe'
-require 'pry'
 
 class PantryTest < Minitest::Test
   def setup
-    recipe_1 = Recipe.new('Cheese Pizza')
-    recipe_1.add_ingredient('Cheese', 20)
-    recipe_1.add_ingredient('Flour', 20)
-    recipe_2 = Recipe.new('Pickles')
-    recipe_2.add_ingredient('Brine', 10)
-    recipe_2.add_ingredient('Cucumbers', 30)
-    recipe_3 = Recipe.new('Peanuts')
-    recipe_3.add_ingredient('Raw nuts', 10)
-    recipe_3.add_ingredient('Salt', 10)
+    recipe1 = Recipe.new('Cheese Pizza')
+    recipe1.add_ingredient('Cheese', 20)
+    recipe1.add_ingredient('Flour', 20)
+    recipe2 = Recipe.new('Pickles')
+    recipe2.add_ingredient('Brine', 10)
+    recipe2.add_ingredient('Cucumbers', 30)
+    recipe3 = Recipe.new('Peanuts')
+    recipe3.add_ingredient('Raw nuts', 10)
+    recipe3.add_ingredient('Salt', 10)
     @pantry = Pantry.new
-    @pantry.add_to_cookbook(recipe_1)
-    @pantry.add_to_cookbook(recipe_2)
-    @pantry.add_to_cookbook(recipe_3)
+    @pantry.add_to_cookbook(recipe1)
+    @pantry.add_to_cookbook(recipe2)
+    @pantry.add_to_cookbook(recipe3)
     @pantry.restock('Cheese', 10)
     @pantry.restock('Flour', 20)
     @pantry.restock('Brine', 40)
@@ -84,14 +85,14 @@ class PantryTest < Minitest::Test
 
   def test_it_can_print_the_shopping_list
     r = Recipe.new('Cheese Pizza')
-    r.add_ingredient("Cheese", 20)
-    r.add_ingredient("Flour", 20)
+    r.add_ingredient('Cheese', 20)
+    r.add_ingredient('Flour', 20)
     pantry = Pantry.new
     pantry.add_to_shopping_list(r)
     r = Recipe.new('Spaghetti')
-    r.add_ingredient("Spaghetti Noodles", 10)
-    r.add_ingredient("Marinara Sauce", 10)
-    r.add_ingredient("Cheese", 5)
+    r.add_ingredient('Spaghetti Noodles', 10)
+    r.add_ingredient('Marinara Sauce', 10)
+    r.add_ingredient('Cheese', 5)
     pantry.add_to_shopping_list(r)
     expected = "* Cheese: 25\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10"
     assert_equal expected, pantry.print_shopping_list
@@ -109,17 +110,17 @@ class PantryTest < Minitest::Test
     r1.add_ingredient('Flour', 20)
     pantry = Pantry.new
     pantry.add_to_cookbook(r1)
-    expected = { 'Cheese Pizza'=> {'Cheese'=> 20, 'Flour'=> 20} }
+    expected = { 'Cheese Pizza' => { 'Cheese' => 20, 'Flour' => 20 } }
     assert_equal expected, pantry.cookbook
   end
 
   def test_what_can_i_make
-    expected = ['Pickles', 'Peanuts']
+    expected = %w[Pickles Peanuts]
     assert_equal expected, @pantry.what_can_i_make
   end
 
   def test_how_many_can_i_make
-    expected = {'Pickles' => 4, 'Peanuts' => 2}
+    expected = { 'Pickles' => 4, 'Peanuts' => 2 }
     assert_equal expected, @pantry.how_many_can_i_make
   end
 end
