56a1a98ec0cb832056f858dfd1790373122cfefc
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-06-05 09:06:46 -0600

    Tests written for pantry

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,23 @@
 class Pantry
 
 end
+
+# ```ruby
+# pantry = Pantry.new
+# # => <Pantry...>
+#
+# # Checking and adding stock
+# pantry.stock
+# # => {}
+#
+# pantry.stock_check("Cheese")
+# # => 0
+#
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+#
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
+# ```
